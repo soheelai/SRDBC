b56e2c6bc4bdace9ce810dd4be3653d1748eaf9a
soheelai <soheelsir@gmail.com>
2026-09-21 01:36:55 +0530

    Update model version from gemini-2.5 to gemini-2.0

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ with tab1:
                         }
                         """
                         response = client.models.generate_content(
-                            model="gemini-2.5-flash",
+                            model="gemini-2.0-flash",
                             contents=[prompt, img]
                         )
                         cleaned_json = response.text.replace("```json", "").replace("```", "").strip()
